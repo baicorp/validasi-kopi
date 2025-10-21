d682f601d4b830bdbab0081f98a55f8b72a7529e
baicorp <bagusatoki@gmail.com>
2025-10-22 00:01:56 +0700

    update /dashboard loading

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return <p>Loading nih. . . </p>;
+  return (
+    <section className="py-3 space-y-4">
+      <div className="w-60 h-6 rounded-md bg-muted animate-pulse my-1"></div>
+    </section>
+  );
 }
