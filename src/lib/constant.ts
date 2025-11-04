2517b683c7b9bb98e1507631758e99e8961f1baa
baicorp <bagusatoki@gmail.com>
2025-11-04 22:53:35 +0700

    change array order

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -31,8 +31,8 @@ const productCategories = [
 ];
 
 const basicExam = [
-  "2 out of 5 campuran kopi",
   "2 out of 5 kopi pure",
+  "2 out of 5 campuran kopi",
   "treshold single",
   "treshold mix",
 ];
