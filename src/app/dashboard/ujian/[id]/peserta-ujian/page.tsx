a3a3f180ab3256a84646f81e4da9086ab53e80bc
baicorp <bagusatoki@gmail.com>
2025-11-11 11:31:48 +0700

    update perserta-ujian page

--- src/app/dashboard/ujian/[id]/peserta-ujian/page.tsx
+++ src/app/dashboard/ujian/[id]/peserta-ujian/page.tsx
@@ -1,12 +1,11 @@
+import { Suspense } from "react";
 import { redirect } from "next/navigation";
-import { validateSessionServer } from "@/actions/validateSession";
-import { getUserRegisteredExamEvents } from "@/actions/examRegistrations";
 import ErrorComp from "@/components/ui/error";
 import Loading from "@/components/skeleton/loading";
-import { Suspense } from "react";
-import { BrushCleaning } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import CodeGroupExamForm from "@/components/ui/addCodeGroupExam";
+import { getExamEventById } from "@/actions/examEvents";
+import ExamParticipants from "@/components/ui/examParticipants";
+import { validateSessionServer } from "@/actions/validateSession";
+import { getAllRegisteredUser } from "@/actions/examRegistrations";
 
 export default async function page({
   params,
@@ -22,83 +21,30 @@ export default async function page({
 
   return (
     <div>
-      <Suspense fallback={<Loading />}>
-        <ExamParticipantList examEventId={id} />
-      </Suspense>
-    </div>
-  );
-}
-
-async function ExamParticipantList({ examEventId }: { examEventId: string }) {
-  const participants = await getUserRegisteredExamEvents(Number(examEventId));
-
-  if ("error" in participants) {
-    return <ErrorComp error={participants.error} />;
-  }
-
-  if (participants.length === 0) {
-    return (
-      <div className="h-80 text-muted-foreground flex justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-2">
-          <BrushCleaning className="w-11 h-11" />
-          <span className="block">Belum ada peserta yang mendaftar.</span>
-        </div>
+      <div className="p-6 border rounded-lg space-y-2">
+        <Suspense key={id} fallback={<Loading />}>
+          <Participants eventId={Number(id)} />
+        </Suspense>
       </div>
-    );
-  }
-
-  return (
-    <div className="flex flex-col gap-4">
-      {participants.map((data) => (
-        <div key={data.examGroup} className="space-y-4 border rounded-lg p-4">
-          <CodeGroupExamForm
-            codeGroupId={data.codeGroupId}
-            selectedExam={data.examGroup}
-            participants={data.data}
-          />
-          <div>
-            <p className="font-medium mb-1">Ujian yang dipilih</p>
-            <div className="flex flex-wrap gap-2">
-              {data.examGroup.split(",").map((exam) => (
-                <Badge key={exam} variant="secondary">
-                  {exam}
-                </Badge>
-              ))}
-            </div>
-          </div>
-          <div>
-            <p className="font-medium mb-1">
-              Daftar Peserta Ujian ({data.data.length} orang)
-            </p>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2">
-              {data.data.map((participant) => (
-                <ExamParticipantItem
-                  key={participant.username}
-                  username={participant.username}
-                  name={participant.name}
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      ))}
     </div>
   );
 }
 
-function ExamParticipantItem({
-  username,
-  name,
-}: {
-  username: string;
-  name: string;
-}) {
+async function Participants({ eventId }: { eventId: number }) {
+  const [examEvent, registeredUsers] = await Promise.all([
+    getExamEventById(eventId),
+    getAllRegisteredUser(eventId),
+  ]);
+
+  if ("error" in examEvent) return <ErrorComp error={examEvent.error} />;
+  if ("error" in registeredUsers)
+    return <ErrorComp error={registeredUsers.error} />;
+
   return (
-    <div className="flex flex-col p-2.5 rounded-md border shadow">
-      <p className="font-medium">{name}</p>
-      <p className="text-sm text-muted-foreground">
-        NIK : <span>{username}</span>
-      </p>
-    </div>
+    <ExamParticipants
+      examEventId={examEvent.id}
+      listParticipant={registeredUsers}
+      totalParticipants={examEvent.totalParticipants}
+    />
   );
 }
