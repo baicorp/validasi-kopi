cea79ea3d092941ce3b0ec1f879db41839a09840
baicorp <bagusatoki@gmail.com>
2025-11-22 13:17:05 +0700

    add suffle function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -121,3 +121,12 @@ export function codeCheck(
 
   return inputValue === examValue ? "correct" : "wrong";
 }
+
+export function shuffle<T>(array: T[]): T[] {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j: number = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+}
