d811ebe54db7540c5b2a20015f7f7eae8c9189f0
baicorp <bagusatoki@gmail.com>
2025-11-25 09:03:26 +0700

    fix hydration error

--- src/components/ui/link.tsx
+++ src/components/ui/link.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 
 export default function InteractiveLink({
@@ -9,14 +10,41 @@ export default function InteractiveLink({
   children,
 }: React.ComponentProps<typeof Link>) {
   const pathname = usePathname();
-  const active = pathname.split("/").slice(-1).toString() === href;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  const hrefString =
+    typeof href === "string"
+      ? href
+      : typeof href === "object" && "pathname" in href
+        ? href.pathname || ""
+        : String(href);
+
+  // avoid hydration mismatch
+  if (!mounted) {
+    return (
+      <Link
+        href={href}
+        className="flex flex-col rounded-md font-medium text-sm hover:text-black"
+      >
+        <span className="px-3 py-1">{children}</span>
+      </Link>
+    );
+  }
+
+  // Handle full vs relative match
+  const lastSegment = pathname?.split("/").pop() || "";
+  const active = hrefString.startsWith("/")
+    ? pathname === hrefString
+    : lastSegment === hrefString;
 
   return (
     <Link
       href={href}
       className={cn(
         "flex flex-col rounded-md font-medium text-sm",
-        active ? "text-black bg-background shadow" : " hover:text-black",
+        active ? "text-black bg-background shadow" : "hover:text-black",
       )}
     >
       <span className="px-3 py-1">{children}</span>
