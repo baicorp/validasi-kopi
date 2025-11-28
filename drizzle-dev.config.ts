330ddf984118d93bbe9d8e5e070d8882b9bcfb30
baicorp <bagusatoki@gmail.com>
2025-11-28 23:15:56 +0700

    add dev drizzle config

--- drizzle-dev.config.ts
+++ drizzle-dev.config.ts
@@ -1,5 +1,8 @@
 import "dotenv/config";
+import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local", override: true });
 
 export default defineConfig({
   out: "./src/db/migration",
