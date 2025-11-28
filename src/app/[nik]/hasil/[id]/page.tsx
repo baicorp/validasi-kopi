7f9737a5d2533fcc8fa426136686372fdef6b71c
baicorp <bagusatoki@gmail.com>
2025-11-28 14:32:14 +0700

    update user exam result page

--- src/app/[nik]/hasil/[id]/page.tsx
+++ src/app/[nik]/hasil/[id]/page.tsx
@@ -10,6 +10,7 @@ import {
 import { toTitleCase } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { getUserLatestExamResult } from "@/actions/examSubmissions";
+import Callout from "@/components/ui/callout";
 
 export default async function Page({
   params,
@@ -27,6 +28,14 @@ export default async function Page({
     <div className="flex justify-center">
       <div className="space-y-8 w-full max-w-3xl">
         <p className="text-xl font-semibold text-center">HASIL UJIAN</p>
+        {data[0]?.numberAttempt === 4 && (
+          <Callout className="border border-blue-200 bg-blue-50 text-blue-500 font-semibold">
+            <p>
+              TERIMAKASIH SUDAH MENYELASAIKAN UJIAN VALIDASI. HASIL UJIAN KE 3
+              (TERAKHIR) AKAN DI INFOKAN BY IM.
+            </p>
+          </Callout>
+        )}
         <Table>
           <TableHeader>
             <TableRow>
@@ -36,9 +45,11 @@ export default async function Page({
               <TableHead className="text-orange-400 bg-muted py-2 md:text-lg">
                 Deskripsi ujian
               </TableHead>
-              <TableHead className="text-center text-orange-400 bg-muted py-2 md:text-lg">
-                Hasil ujian
-              </TableHead>
+              {data[0]?.numberAttempt !== 4 && (
+                <TableHead className="text-center text-orange-400 bg-muted py-2 md:text-lg">
+                  Hasil ujian
+                </TableHead>
+              )}
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -52,17 +63,15 @@ export default async function Page({
                     ? "Reguler"
                     : `Ujian mengulang yang ke ${invoice.numberAttempt - 1} / 3`}
                 </TableCell>
-                <TableCell className="text-center py-4">
-                  <span
-                    className={`text-sm rounded-md px-3 p-1 ${invoice.retake ? "text-red-600 bg-red-100 border border-red-200" : "text-green-600 bg-green-100 border border-green-200"}`}
-                  >
-                    {invoice.retake
-                      ? invoice.numberAttempt === 4
-                        ? "MENGULANG (HABIS)"
-                        : "MENGULANG"
-                      : "LOLOS"}
-                  </span>
-                </TableCell>
+                {invoice.numberAttempt !== 4 && (
+                  <TableCell className="text-center py-4">
+                    <span
+                      className={`text-sm rounded-md px-3 p-1 ${invoice.retake ? "text-red-600 bg-red-100 border border-red-200" : "text-green-600 bg-green-100 border border-green-200"}`}
+                    >
+                      {invoice.retake ? "MENGULANG" : "LOLOS"}
+                    </span>
+                  </TableCell>
+                )}
               </TableRow>
             ))}
           </TableBody>
