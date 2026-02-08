f01e366b82a70ba2be3f131553e486e2caa02a69
baicorp <bagusatoki@gmail.com>
2026-02-08 23:32:03 +0700

    make sure formatLocalTime is using jakarta (utc+7)

--- src/lib/datetimeFormat.ts
+++ src/lib/datetimeFormat.ts
@@ -1,5 +1,8 @@
 export function formatLocalTime(dateTime: string) {
-  return new Date(dateTime).toLocaleString();
+  return new Date(dateTime).toLocaleString("id-ID", {
+    timeZone: "Asia/Jakarta",
+    hour12: true,
+  });
 }
 
 export function getDurationString(start: string, end: string): string {
