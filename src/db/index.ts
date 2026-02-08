f96e823f55d8074880e54842c80201f416da43ec
baicorp <bagusatoki@gmail.com>
2026-02-08 20:23:15 +0700

    remove unused comments

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,16 +9,4 @@ const client = createClient({
 
 const db = drizzle({ client });
 
-// import mysql from "mysql2/promise"; // for mysql
-// import { drizzle } from "drizzle-orm/mysql2"; // for mysql
-
-// const connection = await mysql.createConnection({
-//   host: process.env.MYSQL_DATABASE_HOST!,
-//   user: process.env.MYSQL_DATABASE_USER!,
-//   password: process.env.MYSQL_DATABASE_PASSWORD!,
-//   database: process.env.MYSQL_DATABASE_NAME!,
-// });
-
-// const db = drizzle({ client: connection });
-
 export { db };
