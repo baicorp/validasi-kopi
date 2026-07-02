bf62bbdcc72969dea970de1cbeee151ac27b7a06
baicorp <bagusatoki@gmail.com>
2026-07-02 12:41:01 +0700

    add type for ExamEventPeriode

--- src/components/ui/examEventPeriode.tsx
+++ src/components/ui/examEventPeriode.tsx
@@ -2,17 +2,19 @@ import { cn, toTitleCase } from "@/lib/utils";
 import { examEventPeriode } from "@/lib/types";
 
 export default function ExamEventPeriode({
+  type = "default",
   variant,
   className,
   children,
   ...props
 }: {
+  type?: "small" | "default";
   variant: examEventPeriode;
 } & React.HTMLProps<HTMLDivElement>) {
   return (
     <div
       className={cn(
-        "rounded-full flex justify-center items-center gap-2 px-3 py-1.5",
+        "rounded-full flex justify-center items-center gap-2 px-3 py-1.5 text-xs font-medium",
         {
           "bg-[#B5D4F4] text-[#042C53] border border-[#378ADD]":
             variant === "akan datang",
@@ -21,17 +23,24 @@ export default function ExamEventPeriode({
           "bg-[#C0DD97] text-[#173404] border border-[#639922]":
             variant === "selesai",
         },
+        {
+          "gap-1.5 px-2 py-1 text-[11px] border-none": type === "small",
+        },
       )}
     >
       <div
-        className={cn("w-2 h-2 rounded-full", {
-          "bg-[#185FA5]": variant === "akan datang",
-          "bg-[#854F0B] animate-pulse": variant === "berlangsung",
-          "bg-[#3B6D11]": variant === "selesai",
-        })}
+        className={cn(
+          "w-2 h-2 rounded-full",
+          {
+            "bg-[#185FA5]": variant === "akan datang",
+            "bg-[#854F0B] animate-pulse": variant === "berlangsung",
+            "bg-[#3B6D11]": variant === "selesai",
+          },
+          { "w-1.5 h-1.5": type === "small" },
+        )}
       ></div>
       <div className={className} {...props}>
-        <p className="text-xs font-medium">{toTitleCase(variant)}</p>
+        <p>{toTitleCase(variant)}</p>
         {children}
       </div>
     </div>
